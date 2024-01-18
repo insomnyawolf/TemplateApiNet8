764820bfe100e01fa01d23312167f2b6a338f698
insomnyawolf <insomnyawolf@gmail.com>
2024-01-18 21:03:18 +0100

    Update IEnumerableExtensions.cs

--- AutomaticApiGetSourceGenerator/Helpers/IEnumerableExtensions.cs
+++ AutomaticApiGetSourceGenerator/Helpers/IEnumerableExtensions.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.CodeAnalysis;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Text;
 
 namespace SourceGeneratorHelpers;
 
