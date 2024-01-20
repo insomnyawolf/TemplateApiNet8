2ebaf5271a37f2428709d559df57e78ae07484bd
insomnyawolf <insomnyawolf@gmail.com>
2024-01-20 15:34:01 +0100

    Fix Dev's Stupidity

--- AutomaticApiGetSourceGenerator/EmbedResources/QueryModel.cs
+++ AutomaticApiGetSourceGenerator/EmbedResources/QueryModel.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 using ApiGetGenerator;
 using ApiGetGenerator.Models;
 
@@ -10,8 +9,8 @@ namespace TemplateNamespaceTemplate;
 
 public partial class TemplateQueryModelClassNameTemplate
 {
-    public List<OrderBy<TemplateDatabaseClassColumnsNameTemplate>> OrderBy { get; set; }
-    public List<TemplateDatabaseClassIncludesNameTemplate> Includes { get; set; }
+    public List<OrderBy<TemplateDatabaseClassColumnsNameTemplate>>? OrderBy { get; set; }
+    public List<TemplateDatabaseClassIncludesNameTemplate>? Includes { get; set; }
     public int? PageSize { get; set; }
     public int? PageIndex { get; set; }
 TemplateQueryModelContentTemplate
